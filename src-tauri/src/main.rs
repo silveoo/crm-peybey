7e20d6c735c8eb9a04c5272cadfad350f2d69d82
silveoo <thevolkovmax@gmail.com>
2026-07-15 16:51:30 +0300

    fixed terminal

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,1 +1,3 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main(){tauri::Builder::default().plugin(tauri_plugin_sql::Builder::default().add_migrations("sqlite:crm.db",vec![tauri_plugin_sql::Migration{version:1,description:"initial",sql:include_str!("../migrations/001_initial.sql"),kind:tauri_plugin_sql::MigrationKind::Up},tauri_plugin_sql::Migration{version:2,description:"seed tables",sql:include_str!("../migrations/002_seed_tables.sql"),kind:tauri_plugin_sql::MigrationKind::Up}]).build()).run(tauri::generate_context!()).expect("error while running tauri app")}
